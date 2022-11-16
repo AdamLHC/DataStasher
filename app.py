6152099f0d2207f24c3f1a3b179dd2786636230b
Adam Liu <adamliuhc@outlook.com>
2022-11-16 22:36:14 +0800

    REFACTOR app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask
-from flask_mongoengine import MongoEngine
 
 app = Flask(__name__)
 app.config["MONGODB_SETTINGS"] = [
@@ -11,13 +10,15 @@ app.config["MONGODB_SETTINGS"] = [
         "alias": "default",
     }
 ]
-
 app.config.from_envvar("SECRET_KEY")
+
+from flask_mongoengine import MongoEngine
 
 db = MongoEngine()
 db.init_app(app)
 
 from record import RecordApi
 from file import FileApi
-app.add_url_rule('/record/', view_func=RecordApi.as_view('record_api'))
-app.add_url_rule('/file/<string:id>', view_func=FileApi.as_view('file_api'))
+
+app.add_url_rule("/record/", view_func=RecordApi.as_view("record_api"))
+app.add_url_rule("/file/<string:id>", view_func=FileApi.as_view("file_api"))
